b2f36f0c463277f09e0dd2c187fd4be7883cfc79
haruska <2803+haruska@users.noreply.github.com>
2024-12-01 17:51:01 -0600

    solution to day 1 part 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::iter::zip;
 use aoc_runner_derive::{aoc, aoc_generator};
 
@@ -27,4 +28,22 @@ pub fn solve_part1(input: &(Vec<u32>, Vec<u32>)) -> u32 {
         diff.abs() as u32
     })
         .sum()
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    fn counts(nums: &[u32]) -> HashMap<u32, u32> {
+        nums.iter().fold(HashMap::new(), |mut acc, x| {
+            acc.entry(*x).and_modify(|cnt| *cnt += 1).or_insert(1);
+            acc
+        })
+    }
+
+    let left = counts(input.0.as_slice());
+    let right = counts(input.1.as_slice());
+
+    left.iter().map(|(val, times)| {
+        let cnt = right.get(val).unwrap_or(&0);
+        val * times * cnt
+    }).sum()
 }
