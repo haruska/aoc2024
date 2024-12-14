f30317ee8fee18cbbb13d0df0591d9de78dc8f0a
haruska <2803+haruska@users.noreply.github.com>
2024-12-14 11:32:26 -0600

    input generator

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
+use aoc_runner_derive::aoc_generator;
 use good_lp::{constraint, default_solver, variable, ProblemVariables, Solution, SolverModel};
+use regex::Regex;
 
+#[derive(Default, PartialEq, Clone, Debug)]
 struct Pair {
     x: usize,
     y: usize,
@@ -20,12 +23,14 @@ impl Pair {
     }
 }
 
+#[derive(Default, PartialEq, Clone, Debug)]
 struct ButtonPressCost {
     a: usize,
     b: usize,
     cost: usize,
 }
 
+#[derive(Default, PartialEq, Clone, Debug)]
 struct Machine {
     button_a: Pair,
     button_b: Pair,
@@ -64,9 +69,75 @@ impl Machine {
     }
 }
 
+#[aoc_generator(day13)]
+fn input_generator(input: &str) -> Vec<Machine> {
+    let re = Regex::new(r"X[+=](\d+), Y[+=](\d+)").unwrap();
+
+    input
+        .split("\n\n")
+        .map(|m_input| {
+            let mut parsed = m_input.lines().map(|l| {
+                let res = re.captures(l).unwrap();
+                Pair::new(
+                    res.get(1).unwrap().as_str().parse().unwrap(),
+                    res.get(2).unwrap().as_str().parse().unwrap(),
+                )
+            });
+
+            Machine {
+                button_a: parsed.next().unwrap(),
+                button_b: parsed.next().unwrap(),
+                prize: parsed.next().unwrap(),
+            }
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
+
+    const TEST_INPUT: &str = indoc! {"
+        Button A: X+94, Y+34
+        Button B: X+22, Y+67
+        Prize: X=8400, Y=5400
+
+        Button A: X+26, Y+66
+        Button B: X+67, Y+21
+        Prize: X=12748, Y=12176
+
+        Button A: X+17, Y+86
+        Button B: X+84, Y+37
+        Prize: X=7870, Y=6450
+
+        Button A: X+69, Y+23
+        Button B: X+27, Y+71
+        Prize: X=18641, Y=10279
+    "};
+
+    #[test]
+    fn test_input_generator() {
+        let input = input_generator(TEST_INPUT);
+
+        assert_eq!(input.len(), 4);
+
+        let exp = Machine {
+            button_a: Pair::new(94, 34),
+            button_b: Pair::new(22, 67),
+            prize: Pair::new(8400, 5400),
+        };
+
+        assert_eq!(input[0], exp);
+
+        let exp = Machine {
+            button_a: Pair::new(26, 66),
+            button_b: Pair::new(67, 21),
+            prize: Pair::new(12748, 12176),
+        };
+
+        assert_eq!(input[1], exp);
+    }
 
     #[test]
     fn test_puzzle_one() {
